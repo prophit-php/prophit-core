8d5073c97c74ab0cc5a1f595ea0ae34bcf920c30
elazar <tobias382@gmail.com>
2024-04-13 16:45:40 -0500

    Remove User interface, rename SimpleUser class to User

--- src/User/User.php
+++ src/User/User.php
@@ -2,9 +2,41 @@
 
 namespace Prophit\Core\User;
 
-interface User
+class User
 {
-    public function getId(): string;
+    public function __construct(
+        private string $id,
+        private string $displayName,
+        private UserStatus $status,
+    ) { }
 
-    public function getDisplayName(): string;
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->displayName;
+    }
+
+    public function getStatus(): UserStatus
+    {
+        return $this->status;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === UserStatus::Active;
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->status === UserStatus::Deleted;
+    }
+
+    public function isLocked(): bool
+    {
+        return $this->status === UserStatus::Locked;
+    }
 }
--- tests/User/UserFactory.php
+++ tests/User/UserFactory.php
@@ -5,7 +5,6 @@ namespace Prophit\Core\Tests\User;
 use function Pest\Faker\fake;
 
 use Prophit\Core\User\{
-    SimpleUser,
     User,
     UserStatus,
 };
@@ -27,6 +26,6 @@ class UserFactory
         $id ??= (string) ++$this->lastId;
         $displayName ??= fake()->firstName();
         $status ??= UserStatus::Active;
-        return new SimpleUser($id, $displayName, $status);
+        return new User($id, $displayName, $status);
     }
 }
